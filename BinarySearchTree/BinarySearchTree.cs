2986c6680a5b3a66a97cf14582a3d5ad36678a9b
Denis Zavertyayev <denisz2277@gmail.com>
2021-03-02 13:52:18 -0800

    Created the first part of the logic for the Add method to compare the first Node and move from their based on the size of date being passed in. This was achieved by comparing 'better' data types through dot notation, .Data

--- BinarySearchTree/BinarySearchTree.cs
+++ BinarySearchTree/BinarySearchTree.cs
@@ -23,7 +23,20 @@ namespace BinarySearchTree
 
         public void AddNode(Node nodeToAdd)
         {
-
+            if (RootNode == null) //SF: since the rootnode is the first not you start with it, ADD to it first
+            {
+                RootNode = nodeToAdd;
+            }
+            else
+            {
+                Node currentNode = RootNode;
+                while (true)
+                if (currentNode.leftNode == null && currentNode.Data > nodeToAdd.Data) //SF: to make this work
+                {                                                                         // need to compare better data that they both share (like int =) )
+                        currentNode.leftNode = nodeToAdd;
+                        break;
+                }
+            }
         }
 
         public void SearchNodes(int datatoFind) // this returns a bool (true or false)
