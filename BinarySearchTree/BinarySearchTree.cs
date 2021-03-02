470f6d21e871665261384dbb33f43c890bb39332
Denis Zavertyayev <denisz2277@gmail.com>
2021-03-02 11:11:19 -0800

    init

---
+++ BinarySearchTree/BinarySearchTree.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    public class BinarySearchTree
+    {
+
+        // memeber variables
+
+        // constructor
+        public BinarySearchTree()
+        {
+
+        }
+
+        // member methods 
+
+    }
+}
---
+++ BinarySearchTree/Node.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    class Node
+    {
+
+        //member variables  (has a)
+
+        
+
+        //constructor
+
+
+        //member methods (can do)
+
+    }
+}
